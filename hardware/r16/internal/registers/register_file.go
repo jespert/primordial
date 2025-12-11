cfd06a34e4e676a6dc47bbdaee7ce8dc6d8f4410
Javier Espert Real <javi@espert.dev>
2025-12-11 20:06:02 +0000

    r16: isolate registerFile into a class

---
+++ hardware/r16/internal/registers/register_file.go
@@ -0,0 +1,32 @@
+package registers
+
+import "github.com/jespert/primordial/internal/quality/assert"
+
+type File struct {
+	values [NumRegisters - 1]int16
+}
+
+func (r *File) Read(register int) int16 {
+	r.assertValidRegister(register)
+
+	if register == 0 {
+		return 0
+	}
+
+	return r.values[register-1]
+}
+
+func (r *File) Write(register int, value int16) {
+	r.assertValidRegister(register)
+
+	if register != 0 {
+		r.values[register-1] = value
+	}
+}
+
+func (r *File) assertValidRegister(register int) {
+	inBounds := register >= 0 && register < NumRegisters
+	assert.Truef(inBounds, "register %d is out of bounds", register)
+}
+
+const NumRegisters = 16
